2e6a423ba184eee97927765f3ad33affb9bc9c3b
jexhsu <jexhsu@gmail.com>
2024-07-07 23:10:23 +0800

    day01

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,6 +1,4 @@
 # 数据库初始化
-# @author <a href="https://github.com/liyupi">程序员鱼皮</a>
-# @from <a href="https://yupi.icu">编程导航知识星球</a>
 # 用户表
 create table user
 (
@@ -21,5 +19,3 @@ create table user
     planetCode   varchar(512)                       null comment '星球编号'
 )
     comment '用户';
-
-# [加入编程导航](https://t.zsxq.com/0emozsIJh) 入门捷径+交流答疑+项目实战+求职指导，帮你自学编程不走弯路
---
+++ src/main/java/com/jexhsu/usercenter/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.jexhsu.usercenter.mapper;
+
+import com.jexhsu.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
